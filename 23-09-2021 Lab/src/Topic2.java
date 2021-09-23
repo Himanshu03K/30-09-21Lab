88f8f5db99393d68424277e470f1bd153b0388f1
Himanshu Kushwaha <kushwaha0312@gmail.com>
2021-09-23 15:31:33 +0530

    Final Topic

---
+++ 23-09-2021 Lab/src/Topic2.java
@@ -0,0 +1,17 @@
+
+class Abc
+{
+    // public static final float pi = 3.14f;
+    // In java it is not compulsary to initailize the final variable at the time of declare
+    public static final float pi;
+
+    static {
+        pi=3.14f;
+    }
+}
+
+public class Topic2 {
+    public static void main(String[] args) {
+
+    }
+}
